a5227039ccfc0ee6b2e1b7ab1c832c901eb729e9
Jdbaez94 <javier.d.baez@gmail.com>
2025-05-27 09:00:07 -0500

    feat: :sparkles:

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
+function verificar_delimitadores(expresion) {
+    const stack = [];
+    const delimitadores = {
+        '(': ')',
+        '{': '}',
+        '[': ']',
+        '<': '>'
+    };
 
+    for (let char of expresion) {
+        if (delimitadores[char]) {
+            stack.push(delimitadores[char]);
+        } else if (Object.values(delimitadores).includes(char)) {
+            if (stack.length === 0 || stack.pop() !== char) {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
+}
